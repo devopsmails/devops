7ee89870dc970b85b95afcaa6c35d1534d17fc4a
Devopsmails <119680288+devopsmails@users.noreply.github.com>
2023-06-12 16:57:56 +0530

    Update useful_links.py

--- useful_links.py
+++ useful_links.py
@@ -1,6 +1,8 @@
 Github:
 =======
 DevOps tools installation scripts:
-  https://github.com/vikash-kumar01/installation_scripts/tree/master
+  github: https://github.com/vikash-kumar01/installation_scripts/tree/master
+  youtube: https://www.youtube.com/watch?v=g-v9AsubOqY&list=PLLu1bCv5AByHFz0yycjbp_I5G9NTGF2Cw
+      
     
   
