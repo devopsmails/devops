994048ca69c49bbee8e3ba466d6269a62fc73dc4
Devopsmails <119680288+devopsmails@users.noreply.github.com>
2023-07-20 17:33:07 +0530

    Create shell_scripting.sh

---
+++ shell_scripting.sh
@@ -0,0 +1,5 @@
+What is shell script?
+
+Consist a set of commands to perform a task.
+All the executes sequentially.
+Some tasks like file manipulations, Program Executions, User interactions, Automation Tasks can be done. 
