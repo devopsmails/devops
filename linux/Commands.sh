338c9f450251a6967840b2d1a7663015beb718d3
Devopsmails <119680288+devopsmails@users.noreply.github.com>
2023-08-01 14:29:52 +0530

    Create Commands.sh

---
+++ linux/Commands.sh
@@ -0,0 +1,5 @@
+
+sudo hostnamectl set-hostname JENKINS-SERVER    - To change the hostname to Jenkins_server 
+sudo systemctl reboot                           - To reboot the system
+find / -name java-11*                           - To find the path of a file
+source .bash_profile                            - Update the changes to a file
