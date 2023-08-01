b62c3128120f6b8d9fa7265dade0d45fd4cd20f4
Devopsmails <119680288+devopsmails@users.noreply.github.com>
2023-08-01 14:56:01 +0530

    Update maven.sh

--- Jenkins/installation_script_on_aws/maven.sh
+++ Jenkins/installation_script_on_aws/maven.sh
@@ -10,14 +10,14 @@ mv apache-maven-3.9.3 maven
 cd /opt/maven/bin
 ./mvn -v
 
-#Eviornment variable setup
+#Eviornment variable setup(to mvn command to work & access from anywhere in the server)
 
 sudo su
 cd ~
 ll -a
 find / -name java-11* #copy(/usr/lib/jvm/java-11-openjdk-11.0.19.0.7-1.amzn2.0.1.x86_64)#
 source .bash_profile
-mvn --version
+mvn --v
 
 ./ mvn path setup
 
