c6e073202b903a1f94653cc7d0a51bb81f5c90d2
Devopsmails <119680288+devopsmails@users.noreply.github.com>
2023-08-01 14:28:51 +0530

    Create maven.sh

---
+++ Jenkins/installation_script_on_aws/maven.sh
@@ -0,0 +1,19 @@
+#https://maven.apache.org/download.cgi
+
+sudo su
+cd ~
+cd /opt
+wget https://dlcdn.apache.org/maven/maven-3/3.9.3/binaries/apache-maven-3.9.3-bin.tar.gz
+ll
+tar -xvzf apache-maven-3.9.3-bin.tar.gz
+mv apache-maven-3.9.3 maven
+cd /opt/maven/bin
+./mvn -v
+
+#Eviornment variable setup
+
+sudo su
+cd ~
+ll -a
+find / -name java-11* #copy(/usr/lib/jvm/java-11-openjdk-11.0.19.0.7-1.amzn2.0.1.x86_64)#
+source .bash_profile
