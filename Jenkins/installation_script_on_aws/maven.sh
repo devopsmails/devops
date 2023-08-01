325d16bf1e4ba1f616e398fedc4f29ecb0bde378
Devopsmails <119680288+devopsmails@users.noreply.github.com>
2023-08-01 14:31:24 +0530

    Update maven.sh

--- Jenkins/installation_script_on_aws/maven.sh
+++ Jenkins/installation_script_on_aws/maven.sh
@@ -18,3 +18,15 @@ ll -a
 find / -name java-11* #copy(/usr/lib/jvm/java-11-openjdk-11.0.19.0.7-1.amzn2.0.1.x86_64)#
 source .bash_profile
 mvn --version
+
+./ mvn path setup
+
+M2_HOME=/opt/maven
+M2=/opt/maven/bin
+JAVA_HOME=/usr/lib/jvm/java-11-openjdk-11.0.19.0.7-1.amzn2.0.1.x86_64
+
+# User specific environment and startup programs
+
+PATH=$PATH:$HOME/bin:$JAVA_HOME:$M2_HOME:$M2
+.\
+
