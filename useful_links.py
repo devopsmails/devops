19da4866f93631b71ef0751d9534e56b3dbdb5b1
Devopsmails <119680288+devopsmails@users.noreply.github.com>
2023-08-02 12:08:31 +0530

    Update useful_links.py

--- useful_links.py
+++ useful_links.py
@@ -4,12 +4,13 @@ DevOps tools installation scripts:
   github: https://github.com/vikash-kumar01/installation_scripts/tree/master
   youtube: https://www.youtube.com/watch?v=g-v9AsubOqY&list=PLLu1bCv5AByHFz0yycjbp_I5G9NTGF2Cw
 Java project github link:
-  https://github.com/Ashfaque-9x/registration-app.git
+  GH: https://github.com/Ashfaque-9x/registration-app.git
   yt: https://youtu.be/5_s7EmZWz78
+  GH-COMMANDS: https://github.com/Ashfaque-9x/Scripts/blob/main/DevOps-Project-with-Jenkins-Maven-and-Kubernetes.txt
 Download & installation jenkins on AWS:
 ===================================
-https://www.jenkins.io/doc/tutorials/tutorial-for-installing-jenkins-on-AWS/#downloading-and-installing-jenkins
+Dco: https://www.jenkins.io/doc/tutorials/tutorial-for-installing-jenkins-on-AWS/#downloading-and-installing-jenkins
 
 Installing kubectl on aws Linux AMI:
 =====================
-https://docs.aws.amazon.com/eks/latest/userguide/install-kubectl.html
+Doc: https://docs.aws.amazon.com/eks/latest/userguide/install-kubectl.html
