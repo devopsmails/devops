32cfe1ec95afad5365db5eb4b3cd421b3c99c4d1
Devopsmails <119680288+devopsmails@users.noreply.github.com>
2023-08-27 22:51:09 +0530

    Update nodeHealth.sh

--- Shell_scripting/nodeHealth.sh
+++ Shell_scripting/nodeHealth.sh
@@ -9,7 +9,11 @@
 ################
 
 set -x #debug mode 
+set -e #Exit when error 
+set -o #stops when even pipefails (usally when pipe is mentioned only last command in pipe is success it assumes as success). 
 
 df -h
 free -g
 nproc
+
+i
